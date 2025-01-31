6bec5f71d0a735cfc8cd457e62f424a51dbb9ace
WETTENTLLC <WETTENTERTAINMENTLLC@GMAIL.COM>
2025-01-31 00:22:24 -0800

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
         link.addEventListener('click', (e) => {
             e.preventDefault();
             const targetId = link.getAttribute('href').substring(1);
-            document.getElementById(targetId).scrollIntoView({ behavior: 
-'smooth' });
+            document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
         });
     });
 
